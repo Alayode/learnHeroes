e16ef11746c69747237be48683e6b00524c8d312
Chris Samuel <ksamuel.chris@icloud.com>
2016-11-10 11:59:35 -0500

    created a component and add it to proj

---
+++ app/app.module.ts
@@ -0,0 +1,10 @@
+//This is the entry point to your application.
+//This is the minimum requirements for n NG app.
+
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+    imports:      [ BrowserModule ]
+})
+export class AppModule { }
