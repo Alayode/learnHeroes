402a40a7d99f17419d01f02ecd99f9158bff2b98
Chris Samuel <ksamuel.chris@icloud.com>
2016-11-11 16:04:02 -0500

    revised the  app component and app.module

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,14 +4,22 @@ import {Component} from '@angular/core';
     selector: 'my-app',
     template: `
     <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+ <nav>
+     <a routerLink="/dashboard">Dashboard</a>
+     <a routerLink="/heroes">Heroes</a>
+   </nav>
+    <router-outlet></router-outlet>
   `
-})
-export class AppComponent {
-    title = 'Tour of Heroes';
-}
 
-// - Add HeroesComponent to the declarations array of AppModule so Angular recognizes the <my-heroes> tags.
+})
+
+export class AppComponent {
+        title = 'Tour of Heroes';
+    };
+
+
+
+// / - Add HeroesComponent to the declarations array of AppModule so Angular recognizes the <my-heroes> tags.
 // - Add HeroService to the providers array of AppModule because we'll need it in every other view.
 // - Remove HeroService from the HeroesComponent providers array since it has been promoted.
 // - Add the supporting import statements for AppComponent.
