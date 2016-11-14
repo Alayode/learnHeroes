930873699ee5cef47cc46ae1dc10de71a78da01a
Chris Samuel <ksamuel.chris@icloud.com>
2016-11-14 16:30:11 -0500

    added the delete method to the heroe services

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,24 +1,36 @@
 import { Injectable }    from '@angular/core';
 import { Headers, Http } from '@angular/http';
-
 import 'rxjs/add/operator/toPromise';
-
 import { Hero } from './hero';
-
 @Injectable()
 export class HeroService {
-
-    private heroesUrl  = 'app/heroes'; //URL to web api
-
-    private handleError(error: any): Promise<any> {
-        console.error('An error occurred', error); // for demo purposes only
-        return Promise.reject(error.message || error);
-    }
-
-    //create our headers
     private headers = new Headers({'Content-Type': 'application/json'});
-
-    //create our update method
+    private heroesUrl = 'app/heroes';  // URL to web api
+    constructor(private http: Http) { }
+    getHeroes(): Promise<Hero[]> {
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data as Hero[])
+            .catch(this.handleError);
+    }
+    getHero(id: number): Promise<Hero> {
+        return this.getHeroes()
+            .then(heroes => heroes.find(hero => hero.id === id));
+    }
+    delete(id: number): Promise<void> {
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.delete(url, {headers: this.headers})
+            .toPromise()
+            .then(() => null)
+            .catch(this.handleError);
+    }
+    create(name: string): Promise<Hero> {
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+            .toPromise()
+            .then(res => res.json().data)
+            .catch(this.handleError);
+    }
     update(hero: Hero): Promise<Hero> {
         const url = `${this.heroesUrl}/${hero.id}`;
         return this.http
@@ -27,48 +39,8 @@ export class HeroService {
             .then(() => hero)
             .catch(this.handleError);
     }
-
-    //create our delete method
-
-
-    constructor(private http:Http) {}
-
-    //
-    // getHeroesTheOldWay(): Promise<Hero[]> {
-    //     // return Promise.resolve(HEROES)
-    // }
-
-    getHeroes():Promise<Hero[]> {
-
-        return this.http.get(this.heroesUrl)
-            .toPromise()
-            .then(response => response.json().data as Hero[])
-            //error handling at the end this function we catch server failures
-            // to an error handler
-            .catch(this.handleError);
-
-}
-
-    //Import the Hero Symbol and add the following getHeroesSlowly method to the HeroService
-    getHeroesSlowly(): Promise<Hero[]>{
-        return new Promise<Hero[]>(resolve =>
-        setTimeout(resolve,2000)) // delay the promise for 2 secs
-            .then(() => this.getHeroes());
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
     }
-
-    getHero(id: number): Promise<Hero> {
-        return this.getHeroes()
-            .then(heroes => heroes.find(hero => hero.id === id));
-    }
-
-    // create a method in the HeroService class
-    create(name: string): Promise<Hero> {
-        return this.http
-            .post(this.heroesUrl, JSON.stringify({name:name}),
-        {headers: this.headers})
-            .toPromise()
-            .then ( res => res.json().data)
-            .catch(this.handleError);
-    }
-
 }
