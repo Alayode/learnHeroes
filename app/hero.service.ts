a53110e9cc54208a91ef89a541850a6e517097ee
Chris Samuel <ksamuel.chris@icloud.com>
2016-11-14 13:12:12 -0500

    updated the hero.service to use HTTP promises and imported the in-memory-data.service

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,14 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
-
-
 
 @Injectable()
 export class HeroService {
-    getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
+
+    private heroesUrl  = 'app/heroes'; //URL to web api
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
     }
+
+    constructor(private http:Http) {}
+
+    //
+    // getHeroesTheOldWay(): Promise<Hero[]> {
+    //     // return Promise.resolve(HEROES)
+    // }
+
+    getHeroes():promise<Hero[]> {
+
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data as Hero[])
+            //error handling at the end this function we catch server failures
+            // to an error handler
+            .catch(this.handleError);
+
+}
 
     //Import the Hero Symbol and add the following getHeroesSlowly method to the HeroService
     getHeroesSlowly(): Promise<Hero[]>{
